e121cbe8e15e35030023f2ea67bcc4802b573689
Зиля Шагиахметова <zilyasagiahmetova03@mail.ru>
2022-05-06 18:38:07 +0300

    Добавьте файлы проекта.

---
+++ Pharmacy/Manufacture.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pharmacy
+{
+    class Manufacture
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        public static List<Manufacture> GetManufacture()
+        {
+            return new List<Manufacture>()
+            {
+                new Manufacture { Id = 1, Name = "ОЗОН"},
+                new Manufacture { Id = 2, Name = "Хенкел"},
+                new Manufacture { Id = 3, Name = "Екхольм"}
+            };
+        }
+    }
+}
