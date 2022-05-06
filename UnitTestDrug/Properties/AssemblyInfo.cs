08d6f129fb8765219aa3d95d38af288e6cc0baad
Зиля Шагиахметова <zilyasagiahmetova03@mail.ru>
2022-05-06 19:05:22 +0300

    Добавила тесты и первый тест

---
+++ UnitTestDrug/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("UnitTestDrug")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("UnitTestDrug")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("19610a77-ceed-4a3a-8a8e-bc07b88e778b")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ UnitTestDrug/UnitTest1.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using Pharmacy;
+using System.Collections.Generic;
+
+namespace UnitTestDrug
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestDrugName()
+        {
+            List<string> expectedNameDrug = Drug.GetName();
+
+            List<string> actualNameDrug = new List<string>()
+            {
+                "Фенибут",
+                "Смекта",
+                "Рокона",
+                "Зодак",
+                "Лейкопластырь"
+            };
+
+            CollectionAssert.AreEqual(expectedNameDrug, actualNameDrug);
+        }
+    }
+}
